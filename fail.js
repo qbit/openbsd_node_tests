287ea038523d87238350ea947a2222047a4c403e
Aaron Bieber <aaron@bolddaemon.com>
2017-06-19 08:17:29 -0600

    remove ioctl call

--- fail.js
+++ fail.js
@@ -1,7 +1,7 @@
 var p = require('node-pledge');
 var http = require('http');
 
-p.init('stdio rpath wpath ioctl');
+p.init('stdio rpath wpath tty');
 
 console.log('this will output');
 http.get('http://google.com', function(a,b) {
